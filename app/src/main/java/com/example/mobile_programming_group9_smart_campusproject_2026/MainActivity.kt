d422f172fc6ce53dbc8998d45d37d7daa266a794
lomeling-cmd <lomeling.peter@stud.ndejjeuniversity.ac.ug>
2026-04-30 19:41:00 +0300

    theme Color.kt commit

---
+++ app/src/main/java/com/example/mobile_programming_group9_smart_campusproject_2026/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mobile_programming_group9_smart_campusproject_2026
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mobile_programming_group9_smart_campusproject_2026.ui.screens.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+        composable("details") { RideDetailsScreen(navController) }
+        composable("tracking") { TrackingScreen(navController) }
+        composable("profile") { ProfileScreen(navController) }
+    }
+}
