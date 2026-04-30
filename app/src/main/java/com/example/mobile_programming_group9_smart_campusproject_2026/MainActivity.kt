cbb208daa134131cd689e658ef60a832c6343958
lomeling-cmd <lomeling.peter@stud.ndejjeuniversity.ac.ug>
2026-05-01 00:42:33 +0300

    Color.kt commit

--- app/src/main/java/com/example/mobile_programming_group9_smart_campusproject_2026/MainActivity.kt
+++ app/src/main/java/com/example/mobile_programming_group9_smart_campusproject_2026/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,13 +11,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_programming_group9_smart_campusproject_2026.ui.screens.*
+import com.example.mobile_programming_group9_smart_campusproject_2026.ui.theme.SmartCampusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            SmartCampusTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     AppNavigation()
                 }
             }
