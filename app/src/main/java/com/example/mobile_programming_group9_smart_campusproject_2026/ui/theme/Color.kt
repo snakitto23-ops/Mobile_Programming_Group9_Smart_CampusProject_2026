b0e1df19706e3463655c1da64ed98c20e1af988c
mugwanyanamutete-web <mugwanya.namutete@stud.ndejjeuniversity.ac.ug>
2026-05-01 15:54:31 +0300

    Color.kt update commit

--- app/src/main/java/com/example/mobile_programming_group9_smart_campusproject_2026/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobile_programming_group9_smart_campusproject_2026/ui/theme/Color.kt
@@ -8,3 +8,9 @@ val OrangeTertiary = Color(0xFFFFE0B2)
 
 val DarkOrange = Color(0xFFE65100)
 val LightOrange = Color(0xFFFFF3E0)
+
+val NavyBlue = Color(0xFF0D3B66)
+val GrayText = Color(0xFF9E9E9E)
+val BackgroundLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
